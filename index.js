2c96f4cffa8b33c0dafb758ec4e9f445ed09ebcd
Sevenworks <91027492+SevenworksDev@users.noreply.github.com>
2022-09-07 22:19:56 -0500

    Fix

--- index.js
+++ index.js
@@ -2,48 +2,9 @@ const request = require('request');
 const config = require('./config.json');
 const b64 = require('nodejs-base64-encode');
 const crypto = require('./crypto.js');
-const colours = {
-    reset: "\x1b[0m",
-    bright: "\x1b[1m",
-    dim: "\x1b[2m",
-    underscore: "\x1b[4m",
-    blink: "\x1b[5m",
-    reverse: "\x1b[7m",
-    hidden: "\x1b[8m",
-    
-    fg: {
-        black: "\x1b[30m",
-        red: "\x1b[31m",
-        green: "\x1b[32m",
-        yellow: "\x1b[33m",
-        blue: "\x1b[34m",
-        magenta: "\x1b[35m",
-        cyan: "\x1b[36m",
-        white: "\x1b[37m",
-        crimson: "\x1b[38m" // Scarlet
-    },
-    bg: {
-        black: "\x1b[40m",
-        red: "\x1b[41m",
-        green: "\x1b[42m",
-        yellow: "\x1b[43m",
-        blue: "\x1b[44m",
-        magenta: "\x1b[45m",
-        cyan: "\x1b[46m",
-        white: "\x1b[47m",
-        crimson: "\x1b[48m"
-    }
-};
 
-setTimeout(function() {
-   console.log(colours.bg.blue, colours.fg.white, "Recent Tab Comment Spammer.", colours.reset)
-}, 3000);
-
-setTimeout(function() {
-   console.log(colours.bg.magneta, colours.fg.white, colours.reset)
-}, 6000);
-
-setTimeout(function() {
+console.log("Recent Tab Comment Bot")
+console.log("Written by TheDreamFoxy, Glitzah and Sevenworks.")
 setInterval(function(){fetchRecents();}, config.timing);
 
 var defaults = {
@@ -60,7 +21,7 @@ var defaults = {
 fetchRecents()
 
 function fetchRecents(){
-console.log(colours.bg.black, colours.fg.white, "Fetching", colours.reset)
+console.log("Fetching...");
 request.post({
     url: 'http://www.boomlings.com/database/getGJLevels21.php',
 
@@ -86,12 +47,13 @@ request.post({
 
     }, function (err, httpResponse, body) {
         if(err){
-            console.log(colours.bg.red, colours.fg.white, "Error! Make sure your info is in config.json properly and is correct or wait a moment in case of cooldown.", colours.reset)
+            console.log("Error!");
+            console.log(err.message);
         }else{
             let args = body.split(':');
 
-            console.log(colours.bg.green, colours.fg.white, 'Got level ID: '+args[1], colours.reset);
-            console.log(colours.bg.magneta, colours.fg.white, 'Posting Comment...', colours.reset);
+            console.log('Got level ID: '+args[1]);
+            console.log('Posting Comment...');
             postComment(args[1]); //------------------------------------------------
         }
 });
@@ -120,22 +82,23 @@ function postComment(lvlID){
             if(err){
                 console.log(err.message);
             }else if(httpResponse.statusCode == 500){
-                console.log(colours.bg.red, colours.fg.white, 'Error 500 - Servers are down or you got ratelimited.', colours.reset);
+                console.log('Error!');
+                console.log('500 - Server Side Problem.\nDid you configure the timing in milliseconds and make it over 20000?');
             }else{
-                console.log(colours.bg.green, colours.fg.white, 'You got comment number: '+body+'\n', colours.reset);
+                console.log('You got comment number: '+body+'\n');
             }
         }
     );
 }
 
- function chk(id){
+function chk(id){
 
-   let chk = defaults.userName + defaults.comment + id + defaults.percent + "0xPT6iUrtws0J"
-   chk = crypto.sha1(chk)
-   chk = crypto.encrypt(chk, 29481)
-   return chk
+    let chk = defaults.userName + defaults.comment + id + defaults.percent + "0xPT6iUrtws0J"
+    chk = crypto.sha1(chk)
+    chk = crypto.encrypt(chk, 29481)
+    console.log('CHK hash: '+chk);
+    return chk
 
 }
 
 module.exports.defaults = defaults
-}, 9000);
